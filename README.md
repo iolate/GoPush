1eb8150725ca7ae9906c28ca54f0c156a048c4e2
iolate <iolate@me.com>
2016-06-06 07:18:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+GoPush
+====
+
+Simple asynchronous APNS / GCM service
+
+Example
+--------
+
+Start the service
+
+```bash
+./GoPush --conf ./apps.conf
+```
---
+++ main.go
@@ -0,0 +1,302 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"flag"
+	"time"
+	"path"
+	"io"
+	"io/ioutil"
+	"bytes"
+	"errors"
+	"strconv"
+	"net/http"
+	"crypto/tls"
+	"encoding/json"
+	"golang.org/x/net/http2"
+	"github.com/alexjlockwood/gcm"
+)
+
+const (
+	AppTypeAPNS				= "apns"
+	AppTypeSandboxAPNS		= "apns_sandbox"
+	AppTypeGCM				= "gcm"
+	
+	APNSServer					= "https://api.push.push.apple.com"
+	DevelopmentAPNSServer		= "https://api.development.push.apple.com"
+)
+type AppSettings struct {
+	Host		string
+	Port		string
+	ConfPath	string
+	BaseDir		string
+}
+type App struct {
+	Id			string
+	Type		string	`json:"type"`
+	Key			string	`json:"key"`
+}
+type APNSHeaders struct {
+	Id			string	`json:"apns-id"`
+	Expiration	string	`json:"apns-expiration"`
+	Priority	string	`json:"apns-priority"`
+	Topic		string	`json:"apns-topic"`
+}
+type APNSResponse struct {
+	Reason		string	`json:"reason"`
+	Timestamp	string	`json:"timestamp"`
+}
+type APNSFeedback struct {
+	StatusCode	int		`json:"status_code"`	//410
+	ApnsId		string	`json:"apns-id"`
+	Token		string	`json:"token"`
+	Reason		string	`json:"reason"`
+	Timestamp	string	`json:"timestamp"`
+}
+type GCMFeedback struct {
+	Token		string	`json:"token"`
+	Canonical	string	`json:"canonical"`
+	Reason		string	`json:"reason"`			//NotRegistered
+}
+var apnsFeedbacks map[string][]APNSFeedback = make(map[string][]APNSFeedback)
+var mutexApns sync.Mutex
+var gcmFeedbacks map[string][]GCMFeedback = make(map[string][]GCMFeedback)
+var mutexGcm sync.Mutex
+
+func PrintLog(msg... interface{}) {
+	fmt.Print("[", time.Now().Format(time.RFC3339), "] ")
+	fmt.Println(msg...)
+}
+var pushApps map[string]App
+var appSettings AppSettings
+func main() {
+	PrintLog("[Info/main] GoPush Start!!")
+	
+	flag.StringVar(&appSettings.Host, "host", "127.0.0.1", "Listening IP (default: 127.0.0.1)")
+	flag.StringVar(&appSettings.Port, "port", "5481", "Listening Port (default: 5481)")
+	flag.StringVar(&appSettings.ConfPath, "conf", "", "Configure JSON File Path (required)")
+	flag.Parse()
+	
+	if appSettings.ConfPath == "" {
+		PrintLog("[Error/main] No Configure File!")
+		return
+	}
+	PrintLog("[Info/main] Load config ", appSettings.ConfPath)
+	
+	file, e := ioutil.ReadFile(appSettings.ConfPath)
+	if e != nil {
+		PrintLog("[Error/main] Read file - ", e)
+		return
+	}
+	appSettings.BaseDir = path.Dir(appSettings.ConfPath)
+	
+	err := json.Unmarshal(file, &pushApps)
+	if err != nil {
+		PrintLog("[Info/Error/main] Configure unmarshal - ", e)
+		return
+	}
+	for key, val := range pushApps { val.Id = key}
+	
+	PrintLog("[Info/main] Listening Host: ", appSettings.Host)
+	PrintLog("[Info/main] Listening Port: ", appSettings.Port)
+	
+	http.HandleFunc("/send", send)
+	http.HandleFunc("/feedback", feedback)
+	err = http.ListenAndServe(appSettings.Host+":"+appSettings.Port, nil)
+	if err != nil {
+		PrintLog("[Error/main] Listen - ", e)
+		return
+	}
+}
+
+//======================================================================================//
+func send(w http.ResponseWriter, r *http.Request) {	
+	app := r.FormValue("app")
+	
+	var output string
+	if app, ok := pushApps[app]; ok {
+		payload := r.FormValue("payload")
+		token := r.FormValue("token")
+		
+		if payload == "" || token == ""{
+			w.WriteHeader(http.StatusBadRequest)
+			output = "No payload or token"
+		}else{
+			w.WriteHeader(http.StatusOK)
+			output = "success"
+			go func() {
+				switch app.Type {
+				case AppTypeAPNS: fallthrough
+				case AppTypeSandboxAPNS:
+					headersStr := r.FormValue("headers")
+					var headers *APNSHeaders = nil
+					if headersStr != "" {
+						headers = new(APNSHeaders)
+						err := json.Unmarshal([]byte(headersStr), headers)
+						if err != nil {
+							PrintLog("[Error/send] headers: ", err)
+							headers = nil
+						}
+					}
+					
+					err := SendAPNS(&app, token, payload, headers)
+					if err != nil {
+						PrintLog("[Error/send] SendAPNS: ", err)
+					}
+				case AppTypeGCM:
+					err := SendGCM(&app, token, payload)
+					if err != nil {
+						PrintLog("[Error/send] SendGCM: ", err)
+					}
+				}
+			}()
+		}
+	}else{
+		w.WriteHeader(http.StatusNotFound)
+		output = "Unknown app"
+	}
+	
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Length", strconv.Itoa(len(output)))
+	io.WriteString(w, output)
+}
+
+func feedback(w http.ResponseWriter, r *http.Request) {	
+	app := r.FormValue("app")
+	
+	var output string
+	if app, ok := pushApps[app]; ok {
+		w.WriteHeader(http.StatusOK)
+		output = "null"
+		
+		if app.Type == AppTypeGCM {
+			mutexGcm.Lock()
+			dumps, err := json.Marshal(gcmFeedbacks[app.Id])
+			gcmFeedbacks[app.Id] = nil
+			mutexGcm.Unlock()
+			if err == nil { output = string(dumps) }
+		}else{
+			mutexApns.Lock()
+			dumps, err := json.Marshal(apnsFeedbacks[app.Id])
+			apnsFeedbacks[app.Id] = nil
+			mutexApns.Unlock()
+			if err == nil { output = string(dumps) }
+		}
+		if output == "null" { output = "[]" }
+		
+		w.Header().Set("Content-Type", "application/json")
+	}else{
+		w.WriteHeader(http.StatusNotFound)
+		output = "Unknown App"
+		w.Header().Set("Content-Type", "text/html")
+	}
+	
+	w.Header().Set("Content-Length", strconv.Itoa(len(output)))
+	io.WriteString(w, output)
+}
+
+//======================================================================================//
+func SendAPNS(app *App, token, payload string, headers *APNSHeaders) error {
+	var url string
+	if app.Type == AppTypeAPNS { 
+		url = APNSServer
+	}else{ 
+		url = DevelopmentAPNSServer
+	}
+	url += "/3/device/" + token
+	
+	if app.Key == "" {
+		return errors.New("No Cert/Key File")
+	}
+	
+	var keyPath string
+	if app.Key[:1] == "/" {
+		keyPath = app.Key
+	}else if len(app.Key) > 2 && app.Key[:2] == "./" {
+		keyPath = appSettings.BaseDir + app.Key[1:]
+	}else {
+		keyPath = appSettings.BaseDir + "/" + app.Key
+	}
+	
+	cert, err := tls.LoadX509KeyPair(keyPath, keyPath)
+	if err != nil { return err }
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		InsecureSkipVerify: true,
+	}
+	tr := &http2.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: tr}
+	
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payload)))
+	if err != nil { return err }
+	if headers != nil {
+		if headers.Id != "" { req.Header.Set("apns-id", headers.Id) }
+		if headers.Expiration != "" { req.Header.Set("apns-expiration", headers.Expiration) }
+		if headers.Priority != "" { req.Header.Set("apns-priority", headers.Priority) }
+		if headers.Topic != "" { req.Header.Set("apns-topic", headers.Topic) }
+	}
+	
+	resp, err := client.Do(req)
+	if err != nil { return err }
+	
+	if resp != nil {
+		defer resp.Body.Close()
+		
+		switch resp.StatusCode {
+		case 200:
+			return nil
+		//case 410: //The device token is no longer active for the topic.
+		default:
+			var feedback APNSFeedback
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil { return err }
+			err = json.Unmarshal([]byte(body), &feedback)
+			if err != nil { return err }
+			
+			feedback.StatusCode = resp.StatusCode
+			feedback.ApnsId = resp.Header["Apns-Id"][0]
+			feedback.Token = token
+			
+			mutexApns.Lock()
+			apnsFeedbacks[app.Id] = append(apnsFeedbacks[app.Id], feedback)
+			mutexApns.Unlock()
+		}
+		return nil
+	}else{
+		return errors.New("Response is nil")
+	}
+}
+
+func SendGCM(app *App, token, payload string) error {
+	var payloadMap map[string]interface{}
+	err := json.Unmarshal([]byte(payload), &payloadMap)
+	if err != nil { return err }
+	
+	msg := gcm.NewMessage(payloadMap, token)
+	sender := &gcm.Sender{ApiKey: app.Key}
+	response, err := sender.Send(msg, 2)
+	if err != nil {
+		return err
+	}
+	
+	if response != nil && len(response.Results) > 0 {
+		result := response.Results[0]
+		
+		if result.Error != "" || result.RegistrationID != "" {
+			feedback := GCMFeedback{
+				Token: token,
+				Canonical: result.RegistrationID,
+				Reason: result.Error,
+			}
+			
+			mutexGcm.Lock()
+			gcmFeedbacks[app.Id] = append(gcmFeedbacks[app.Id], feedback)
+			mutexGcm.Unlock()
+		}
+	}else {
+		return errors.New("Response is nil")
+	}
+	
+	return nil
+}
